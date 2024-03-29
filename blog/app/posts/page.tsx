37c5029c22d6347692bab203e45c3c888736ed6a
Joshua Ajayi <kenduajayi@gmail.com>
2024-03-29 00:20:40 -0700

    syntax changed

--- blog/app/posts/page.tsx
+++ blog/app/posts/page.tsx
@@ -19,12 +19,12 @@ const Explorepage = async () => {
   return (
     <div className="w-full flex justify-center items-center lg:p-10 p-3">
       {!user ? (
-        <h1 className="text-3xl font-extrabold">Sign in to view your post!</h1>
+        <h1 className="text-3xl font-extrabold">Sign in to view post!</h1>
       ) : (
         <div className="mt-[180px] w-full">
           <div className="w-full text-start mb-20 flex flex-col gap-10 align-middle items-start">
             <h1 className="lg:text-7xl text-3xl uppercase font-bold">
-              Hello {user?.name}
+              Hello {user?.name} !
             </h1>
             <span className="text-lg">
               You have published {posts.length} posts
